38069ececbe538b15152c0580e19f9118012a6ce
Orcalancelulu <lanceborer1@gmail.com>
2023-03-01 11:31:04 +0100

    charakter menü

--- clientside/menue.js
+++ clientside/menue.js
@@ -1,1 +1,14 @@
-let listElement = document.getElementById("choosePlayer");
+document.getElementById("characterButton").addEventListener("click", changeToCharacterMenu);;
+document.getElementById("done_button").addEventListener("click", changeToCharacterMenu);;
+
+
+function changeToCharacterMenu() {
+    if (document.getElementById("menue").style.display != "none") {
+        document.getElementById("menue").style.display = "none";
+        document.getElementById("chooseCharacters").style.display = "grid";
+
+    } else {
+        document.getElementById("menue").style.display = "grid";
+        document.getElementById("chooseCharacters").style.display = "none";
+    }
+}
