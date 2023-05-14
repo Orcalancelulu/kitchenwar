f7ad9cf24120b92987366b7e8254b9f30242f20f
Orcalancelulu <lanceborer1@gmail.com>
2023-05-14 18:00:36 +0200

    added cron job access so server keeps running

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
 const THREE = require("three");
 const WebSocket = require('ws');
+var http = require('http');
+
 const wss = new WebSocket.Server({ port: 7031 });
+
 
 let clientList = [];
 const scene = new THREE.Scene();
@@ -71,6 +74,12 @@ let mapObject = {
 }
 
 //fertig hardcoded
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('server running');
+}).listen(8081);
 
 function addNewProjectile(projectile) { //OnOrOff: false = turn off, true = turn on
 
